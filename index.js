826069bba5b3cb9776569e1ad1ea52528a28425c
afonte15 <af689@nyu.edu>
2024-06-23 21:18:26 -0400

    tests

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { execSync } = require('child_process');
-const fs = require('fs');
-const path = require('path');
 
 async function run() {
   try {
-    // Check if node_modules folder exists, if not, install dependencies
-    if (!fs.existsSync(path.join(__dirname, 'node_modules'))) {
-      console.log('node_modules not found, installing dependencies...');
-      execSync('npm install', { stdio: 'inherit' });
-    }
-
     const repo = core.getInput('repo');
     const eventType = core.getInput('event-type');
     const clientPayload = JSON.parse(core.getInput('client-payload'));
-    //const repoToken = core.getInput('repo-token');
+    const repoToken = core.getInput('repo-token');
 
     const [owner, repoName] = repo.split('/');
 
-    const octokit = github.getOctokit();
+    const octokit = github.getOctokit(repoToken);
 
     await octokit.rest.repos.createDispatchEvent({
       owner,
